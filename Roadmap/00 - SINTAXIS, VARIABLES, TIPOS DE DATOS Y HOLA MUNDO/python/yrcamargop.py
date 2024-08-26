96d77928ea9cb733d4eecbf1970bfac5722e9aa8
yrcamargop <ycamargop1@gmail.com>
2024-08-26 09:21:09 -0500

    Create yrcamargop.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yrcamargop.py
@@ -0,0 +1,15 @@
+# https://python.org/
+
+# Comentario en una linea
+
+"""
+Esto tambien es 
+un comentario 
+en varias lineas
+"""
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convencion
+MY CONSTANT = "FSDJFKSDJHFSKEFH"
